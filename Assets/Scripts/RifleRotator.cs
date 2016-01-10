603b61808556b5526b503234ea93a96d960772c0
villj2 <villj2@bfh.ch>
2016-01-10 10:12:58 +0100

    Rifle fixed to Wand

--- Assets/Scripts/RifleRotator.cs
+++ Assets/Scripts/RifleRotator.cs
@@ -8,21 +8,25 @@ public class RifleRotator : MonoBehaviour {
 	void Start () {
 
         //transform.parent.parent.parent = API.Instance.Cave.CameraContainer.transform;
-        transform.parent.parent.parent = API.Instance.CameraContainer.transform;
-        transform.parent.parent.localPosition = Vector3.zero;
+        
+        ////transform.parent.parent.parent = API.Instance.CameraContainer.transform;
+        ////transform.parent.parent.localPosition = Vector3.zero;
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        // Get relative rotation between eyes / wand
-        //var rotFromPlugin = Quaternion.Inverse(API.Instance.Cave.Eyes.transform.rotation) * API.Instance.Cave.Wand.transform.rotation;
-        var rotFromPlugin = API.Instance.AngleWandEyes;
+        //// Get relative rotation between eyes / wand
+        ////var rotFromPlugin = Quaternion.Inverse(API.Instance.Cave.Eyes.transform.rotation) * API.Instance.Cave.Wand.transform.rotation;
+        //var rotFromPlugin = API.Instance.AngleWandEyes;
 
-        // Rotate RotatorEyes
-        transform.parent.parent.localRotation = API.Instance.Eyes.transform.rotation;
+        //// Rotate RotatorEyes
+        //transform.parent.parent.localRotation = API.Instance.Eyes.transform.rotation;
 
-        // Rotate RotatorWand
-        transform.parent.localRotation = rotFromPlugin;
+        //// Rotate RotatorWand
+        //transform.parent.localRotation = rotFromPlugin;
+
+        transform.localPosition = API.Instance.Wand.transform.localPosition;
+        transform.localRotation = API.Instance.Wand.transform.localRotation;
     }
 }
